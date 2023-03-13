600802f9cca1ae2bf9fc698648b7ea2d70c9b1e5
Moongazing <72943708+Moongazing@users.noreply.github.com>
2023-03-14 02:51:26 +0300

    Add project files.

---
+++ TAO.Blog.Core/Models/Article.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAO.Blog.Core.Models
+{
+    public class Article:BaseEntity
+    {
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public string? Title { get; set; }
+        public string? Subject { get; set; }
+        public int NumberOfLetters { get; set; }
+
+    }
+}
---
+++ TAO.Blog.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAO.Blog.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task GetByIdAsync(int id);
+        IQueryable<T> GetAll();
+        IQueryable<T> Where(Expression<Func<T>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<bool> AnyAsync(Expression<Func<T,bool>> expression);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+    }
+}
